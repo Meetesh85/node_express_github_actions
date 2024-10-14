0906ca8061dd499a5ed881e79f8b534ef4975b38
Meetesh85 <miteshraj456@gmail.com>
2024-10-14 19:35:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,45 @@
-var express = require('express')
+var express = require('express');
 
-var app = express()
+var app = express();
 
-const SERVER_PORT = 3000
-const SERVER_HOST = "localhost"
+const SERVER_PORT = 3000;
+const SERVER_HOST = "localhost";
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
-//http://localhost:3000/
+// Home Page Route
 app.get('/', function (req, res) {
-    res.send("<h1>Home Page</h1>")
-})
-
-//http://localhost:3000/profile
-app.post('/profile', (req, res) => {
-  console.log(req.body)
-  res.json(req.body)
-})
-
-//http://localhost:3000/admin
-app.get('/admin', (req, res) => {
-  res.send('Admin Homepage')
-})
-
-//http://localhost:3000/user/100
-app.get("/user/:id", (req, res)=> {
-      res.send(`User ID: ${req.params.id}`);
-    }
-)
-
-//http://localhost:3000/valueofday/1980-01-24
-app.get("/valueofday/:year(\\d{4})-:month(\\d{2})-:day(\\d{2})", (req, res) => {
-  console.log(req.params)
-  res.send(req.params)
+    res.send("<h1>C0907704 Meetesh85</h1>");
 });
-  
-app.listen(process.env.PORT  || SERVER_PORT, () => {
+
+// Profile Route
+// http://localhost:3000/profile
+app.post('/profile', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+// Admin Route
+// http://localhost:3000/admin
+app.get('/admin', (req, res) => {
+    res.send('Admin Homepage');
+});
+
+// User ID Route
+// http://localhost:3000/user/100
+app.get("/user/:id", (req, res) => {
+    res.send(`User ID: ${req.params.id}`);
+});
+
+// Value of Day Route
+// http://localhost:3000/valueofday/1980-01-24
+app.get("/valueofday/:year(\\d{4})-:month(\\d{2})-:day(\\d{2})", (req, res) => {
+    console.log(req.params);
+    res.send(req.params);
+});
+
+// Start the server
+app.listen(process.env.PORT || SERVER_PORT, () => {
     console.log(`Server running at http://${SERVER_HOST}:${SERVER_PORT}/`);
-})
+});
